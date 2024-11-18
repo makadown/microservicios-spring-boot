1b22bcce8c70df1ce5fd2dc44bfb0e08a3e61584
makadown <maseflores79@gmail.com>
2024-11-18 15:02:26 -0800

    comenzando de cero para 2025

---
+++ msvc-products/src/main/java/com/mario/springcloud/msvc/products/MsvcProductsApplication.java
@@ -0,0 +1,13 @@
+package com.mario.springcloud.msvc.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcProductsApplication.class, args);
+	}
+
+}
---
+++ msvc-products/src/test/java/com/mario/springcloud/msvc/products/msvc_products/MsvcProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mario.springcloud.msvc.products.msvc_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
