9b9f4a4f6f501d69aac7e30e0bfba9ffc4f1aa79
Mario Arturo Serrano Flores <mario.serrano@imssbienestar.gob.mx>
2024-11-20 10:57:23 -0800

    preparando para mysql

---
+++ msvc-products/src/main/java/com/mario/springcloud/msvc/products/controllers/ProductController.java
@@ -0,0 +1,39 @@
+package com.mario.springcloud.msvc.products.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mario.springcloud.msvc.products.entities.Product;
+import com.mario.springcloud.msvc.products.services.ProductService;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class ProductController {
+    final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Product> list() {
+        return this.service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> details(@PathVariable Long id) {
+        Optional<Product> productOptional = service.findById(id);
+
+        if (productOptional.isPresent()) {
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build(); // null;
+    }
+
+}
